d7049dc18f9ecdba5fedc7c665c5d0a6505d70e1
Hyein Lee <gwm0120@gmail.com>
2026-08-20 16:21:27 +0900

    사람인 크롤링 search_mode 오류 수정_2

--- app.py
+++ app.py
@@ -604,7 +604,6 @@ def api_saramin_run():
             header_mode,
             column_index,
             column_letter,
-            search_mode=search_mode,
         )
     except Exception as e:
         return jsonify({"ok": False, "message": f"엑셀 파일을 읽을 수 없습니다: {str(e)}"}), 400
